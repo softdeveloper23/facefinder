bc3d4982ac3fb9a6e9416d326c34776745cace0b
unknown <brannongarrett77@gmail.com>
2023-05-29 15:51:28 +0200

    Added Logo.css file and changed up code in Logo.js to match up with tutorial. Also added imagefinder.png. Everything is working.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Tilt from 'react-parallax-tilt';
+import imagefinder from './imagefinder.png';
+import './Logo.css';
 
 
 const Logo = () => {
     return (
-        <Tilt>
-            <div style={{ height: '300px', backgroundColor: 'darkgreen' }}>
-                <h1>React Parallax Tilt 👀</h1>
-            </div>
-        </Tilt>
+        <div className="ma4 mt0">
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 100, width: 100 }}>
+                <div className="Tilt-inner pa3">
+                    <img style={{ padingTop: '5px' }} alt="logo" src={imagefinder} />
+                </div>
+            </Tilt>
+        </div>
     );
 }
 
