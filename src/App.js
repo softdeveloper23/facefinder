642170d6a08ab0a8500fbe76b2998cf8dd6fe4e3
unknown <brannongarrett77@gmail.com>
2023-05-30 11:52:50 +0200

    Updated App.js and ImageLinkForm.js so that the input box detects input changes.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,24 @@ import Rank from './components/Rank/Rank';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
         {/*<FaceRecognition /> */}
       </div>
     );
